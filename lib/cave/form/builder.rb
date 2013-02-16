481e3c8734a4b030c4e7c89c66353c71e8290267
James Dabbs <jdabbs@emcien.com>
2013-02-16 18:18:19 -0500

    Fix error handling. Re-display bound values.

--- lib/cave/form/builder.rb
+++ lib/cave/form/builder.rb
@@ -4,16 +4,19 @@ module Cave
   class Form
     class Builder < ActionView::Helpers::FormBuilder
       def bootstrap control_type, name
-        label_html = label name, class: 'control-label'
-        field_html = send control_type, name
-
         error = object.errors[name]
-        if object.bound? && error
-          error_class = 'error'
-          error_html  = "<span class='help-inline'>#{error.first}</span>"
+        if object.bound?
+          value = object.send name
+          unless error.empty?
+            error_class = 'error'
+            error_html  = "<span class='help-inline'>#{error.first}</span>"
+          end
         else
-          error_class = error_html = ''
+          value = error_class = error_html = ''
         end
+
+        label_html = label name, class: 'control-label'
+        field_html = send control_type, name, value: value
 
         raw = %{
           #{label_html}
