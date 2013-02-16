b1728dbe7afdabd1cf7233a1d5d7cbd5552cef8a
James Dabbs <jdabbs@emcien.com>
2013-02-16 15:00:21 -0500

    Add bootstrap-aware form builder

--- lib/cave.rb
+++ lib/cave.rb
@@ -2,5 +2,7 @@ require 'cave/errors'
 require 'cave/form'
 require 'cave/model_form'
 
+require 'cave/form/builder'
+
 module Cave
 end
---
+++ lib/cave/form/builder.rb
@@ -0,0 +1,28 @@
+module Cave
+  class Form
+    class Builder < ActionView::Helpers::FormBuilder
+      def bootstrap control_type, name
+        label_html = label name, class: 'control-label'
+        field_html = send control_type, name
+
+        error = object.errors[name]
+        if object.bound? && error
+          error_class = 'error'
+          error_html  = "<span class='help-inline'>#{error.first}</span>"
+        else
+          error_class = error_html = ''
+        end
+
+        raw = %{
+          #{label_html}
+          <div class='controls'>
+            #{field_html}
+            #{error_html}
+          </div>
+        }.squish.html_safe
+
+        @template.content_tag 'div', raw, class: "control-group #{error_class}"
+      end
+    end
+  end
+end
