3f97a4e04bdcfab01889cde0ead22b3ef3b6034d
Steven Clontz <steven.clontz@gmail.com>
2013-11-05 14:15:12 -0600

    Moved bootstrap to bootstrap2, set bootstrap to use 3

--- lib/cave/form/builder.rb
+++ lib/cave/form/builder.rb
@@ -1,7 +1,7 @@
 require 'action_view'
 
 class Cave::Form::Builder < ActionView::Helpers::FormBuilder
-  def bootstrap control_type, field
+  def bootstrap2 control_type, field
     field = object.class.fields[field] if field.is_a?(Symbol)
     
     error = object.errors[field.name]
@@ -26,5 +26,33 @@ class Cave::Form::Builder < ActionView::Helpers::FormBuilder
     }.squish.html_safe
 
     @template.content_tag 'div', raw, class: "control-group #{field.name} #{error_class}"
+  end
+
+  
+  def bootstrap control_type, field
+    field = object.class.fields[field] if field.is_a?(Symbol)
+    
+    error = object.errors[field.name]
+    value = object.lookup field.name
+
+    if error.empty?
+      error_class = error_html = ''
+    else
+      error_class = 'error'
+      error_html  = "<span class='help-block'>#{error.first}</span>"
+    end
+
+    label_html = label field.label, class: 'control-label'
+    field_html = send control_type, field.name, value: value
+
+    raw = %{
+      #{label_html}
+      <div class='form-group'>
+        #{field_html}
+        #{error_html}
+      </div>
+    }.squish.html_safe
+
+    @template.content_tag 'div', raw, class: "form-control #{field.name} #{error_class}"
   end
 end
